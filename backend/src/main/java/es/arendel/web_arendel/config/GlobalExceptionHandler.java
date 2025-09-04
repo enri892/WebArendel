0240522b297fe4dbfd62d3007ef208f6e8f362b3
PurpleZL <vladutorza64@gmail.com>
2025-09-04 17:17:06 +0200

    controlar excepcion de contenido no soportado 415

--- backend/src/main/java/es/arendel/web_arendel/config/GlobalExceptionHandler.java
+++ backend/src/main/java/es/arendel/web_arendel/config/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package es.arendel.web_arendel.config;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.multipart.MaxUploadSizeExceededException;
@@ -11,6 +12,22 @@ import java.util.Map;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    /**
+     * HTTP 415 - Tipo de contenido no soportado
+     */
+    @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
+    public ResponseEntity<Map<String, Object>> handleUnsupportedMediaType(HttpMediaTypeNotSupportedException e) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("success", false);
+        response.put("message", "Tipo de contenido no soportado. Este endpoint requiere multipart/form-data");
+        response.put("supportedMediaTypes", e.getSupportedMediaTypes().toString());
+        response.put("receivedMediaType", e.getContentType() != null ? e.getContentType().toString() : "null");
+        return ResponseEntity.badRequest().body(response);
+    }
+
+    /**
+     * HTTP 413 - Archivo demasiado grande
+     */
     @ExceptionHandler(MaxUploadSizeExceededException.class)
     public ResponseEntity<Map<String, Object>> handleMaxUploadSize(MaxUploadSizeExceededException e) {
         Map<String, Object> response = new HashMap<>();
